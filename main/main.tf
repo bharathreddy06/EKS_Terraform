6667310a774b092188b5d66dd65b3a5b33065765
EC2 Default User <ec2-user@ip-172-31-4-234.ap-south-1.compute.internal>
2023-05-22 10:30:28 +0000

    eks terraform 22-05-2023

--- main/main.tf
+++ main/main.tf
@@ -1,15 +1,41 @@
 module "create_vpc" {
     source = "../modules/network"
+    private_subnets = var.private_subnets
+    public_subnets = var.public_subnets
+    avail_zones = var.avail_zones
+    vpc_block = var.vpc_block
 }
 
 module "create_cluster" {
     source = "../modules/cluster"
-    depends_on = [module.create_vpc]
+    private_subnet_ids = module.create_vpc.private_subnet_ids
+    # private_subnet_id_2 = module.create_vpc.private_subnet_id_2
+    public_subnet_ids =  module.create_vpc.public_subnet_ids
+    # public_subnet_id_2 =  module.create_vpc.public_subnet_id_2
+    cluster_name = var.cluster_name
+    # depends_on = [
+    #     module.create_vpc
+    # ]
 
 }
 
 module "create_worker" {
     source = "../modules/worker"
     depends_on = [module.create_cluster]
+    aws_eks_cluster_name = module.create_cluster.aws_eks_cluster_name
+    # private_subnets = var.private_subnets
+    # cluster_name = var.cluster_name
+    private_subnet_ids = module.create_vpc.private_subnet_ids
+    # private_subnet_id_2 = module.create_vpc.private_subnet_id_2
+    public_subnet_ids =  module.create_vpc.public_subnet_ids
+    # public_subnet_id_2 =  module.create_vpc.public_subnet_id_2
 
 }
+
+# module "k8s_apply_yaml" {
+#     source = "../modules/kubernetes"
+#     # depends_on = [module.create_worker]
+#     aws_eks_cluster_name = module.create_cluster.aws_eks_cluster_name
+#     aws_eks_cluster_endpoint = module.create_cluster.aws_eks_cluster_endpoint
+
+# }
